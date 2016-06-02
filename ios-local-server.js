b9ba80878e89a588e529a40e1d0baa296e3b5f19
Elber Carneiro <elber@yieldmo.com>
2016-06-02 11:12:40 -0400

    created loop for all formats

--- ios-local-server.js
+++ ios-local-server.js
@@ -1,4 +1,4 @@
-/* jshint node: true */
+/* jshint node: true, mocha: true */
 
 // run this script using `mocha ios-local-server.js`
 
@@ -11,101 +11,117 @@ var _ = require('underscore');
 var serverConfigs = require('./helpers/appium-servers');
 var localServer = require('./helpers/local-server');
 
-// START THE DRIVER
-describe('ios local server', function () {
-  this.timeout(300000);
-  var driver;
-  var allPassed = true;
+var formatNames = [
+  'Store 1 Ad', 'Store 2 Ads', 'Store 3 Ads', 'Store 4 Ads',
+  'Domino 1 Ad', 'Domino 2 Ads', 'Domino 3 Ads', 'Domino 4 Ads',
+  'Postcard', 'Hyperscroller', 'Window', 'Pull',
+  'Feature', 'Ticker', 'Wrapper'
+];
 
-  before(function () {
-    localServer.start();
-    var serverConfig = serverConfigs.local;
-    driver = wd.promiseChainRemote(serverConfig);
-    require('./helpers/logging').configure(driver);
+function simpleTestSuite(formatIndex, formatName) {
+  return function () {
+    this.timeout(300000);
+    var driver;
+    var allPassed = true;
 
-    var desired = _.clone(require('./helpers/caps').ios93);
-    desired.app = '/Users/elbercarneiro/Desktop/Demo2.app';
-    if (process.env.npm_package_config_sauce) {
-      desired.name = 'ios - local server';
-      desired.tags = ['sample'];
-    }
+    before(function () {
+      localServer.start();
+      var serverConfig = serverConfigs.local;
+      driver = wd.promiseChainRemote(serverConfig);
+      require('./helpers/logging').configure(driver);
 
-    return driver.init(desired);
-  });
+      var desired = _.clone(require('./helpers/caps').ios93);
+      desired.app = '/Users/elbercarneiro/Desktop/Demo2.app';
+      if (process.env.npm_package_config_sauce) {
+        desired.name = 'ios - local server';
+        desired.tags = ['sample'];
+      }
 
-  // CLOSE DRIVER AND CLEANUP
-  after(function () {
-    localServer.stop();
-    return driver
-      .quit()
-      .finally(function () {
-        if (process.env.npm_package_config_sauce) {
-          return driver.sauceJobStatus(allPassed);
-        }
-      });
-  });
+      return driver.init(desired);
+    });
 
-  afterEach(function () {
-    allPassed = allPassed && this.currentTest.state === 'passed';
-  });
+    // CLOSE DRIVER AND CLEANUP
+    after(function () {
+      localServer.stop();
+      return driver
+        .quit()
+        .finally(function () {
+          if (process.env.npm_package_config_sauce) {
+            return driver.sauceJobStatus(allPassed);
+          }
+        });
+    });
 
-  // scroll to format and click on it, check for modal webview
-  it('Store 1 Ad should open placement webview', function() {
-    return driver
-      .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]')
-      .click()
-      .sleep(1000)
-      .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[18]')
-      .click()
-      .sleep(2000)
-      .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[15]/UIAButton[1]')
-      .click()
-      .sleep(3000)
-      .contexts()
-      .then(function(contexts) {
-        var contextCount = contexts.length;
-        return contextCount.should.equal(2);
-      });
-  });
+    afterEach(function () {
+      allPassed = allPassed && this.currentTest.state === 'passed';
+    });
 
-  // switch to webview context and perform a command
-  it('Store 1 Ad webview context should be available for testing', function() {
-    return driver
-      .context('WEBVIEW_1')
-      .get('http://33.media.tumblr.com/762fa7a08f15cfe87dc05de591cb49eb/tumblr_inline_nyqvseewec1tqozky_500.gif')
-      .sleep(2000)
-      .currentContext()
-      .then(function(context) {
-        return context.should.equal('WEBVIEW_1');
-      });
-  });
+    // scroll to format and click on it, check for modal webview
+    it('Store 1 Ad should open placement webview', function () {
+      // formats 10, 11, and 12 do dot require button presses
+      var button = (formatIndex > 9 && formatIndex < 13) ? '' : '/UIAButton[1]';
 
-  // switch to native context and check status
-  it('Store 1 Ad native app context should be available for testing', function() {
-    return driver
-      .context('NATIVE_APP')
-      .currentContext()
-      .then(function(context) {
-        return context.should.equal('NATIVE_APP');
-      });
-  });
+      return driver
+        .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[' + formatIndex + ']')
+        .click()
+        .sleep(1000)
+        .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[18]')
+        .click()
+        .sleep(2000)
+        .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[15]' + button)
+        .click()
+        .sleep(3000)
+        .contexts()
+        .then(function (contexts) {
+          var contextCount = contexts.length;
+          return contextCount.should.equal(2);
+        });
+    });
 
-  // close webview and check context count
-  it('Store 1 Ad webview should close properly', function() {
-    return driver
-      .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIAToolbar[1]/UIAButton[1]')
-      .click()
-      .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[1]')
-      // .sleep(2000)
-      // .contexts()
-      // .then(function(contexts) {
-      //   var contextCount = contexts.length;
-      //   return contextCount.should.equal(1);
-      // });
-      .currentContext()
-      .then(function(context) {
-        return context.should.equal('NATIVE_APP');
-      });
-  });
+    // switch to webview context and perform a basic command
+    it('Store 1 Ad webview context should be available for testing', function () {
+      return driver
+        .context('WEBVIEW_1')
+        .get('http://33.media.tumblr.com/762fa7a08f15cfe87dc05de591cb49eb/tumblr_inline_nyqvseewec1tqozky_500.gif')
+        .sleep(2000)
+        .currentContext()
+        .then(function (context) {
+          return context.should.equal('WEBVIEW_1');
+        });
+    });
 
-});
+    // switch to native context and check status
+    it('Store 1 Ad native app context should be available for testing', function () {
+      return driver
+        .context('NATIVE_APP')
+        .currentContext()
+        .then(function (context) {
+          return context.should.equal('NATIVE_APP');
+        });
+    });
+
+    // close webview and check context count
+    it('Store 1 Ad webview should close properly', function () {
+      return driver
+        .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIAToolbar[1]/UIAButton[1]')
+        .click()
+        .elementByXPath('//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[1]')
+        .currentContext()
+        .then(function (context) {
+          return context.should.equal('NATIVE_APP');
+        });
+    });
+  };
+}
+
+for (var i = 0; i < formatNames.length; i++) {
+
+  // formatIndex matches UIATableCell index
+  var formatIndex = i + 1;
+
+  // run the test
+  describe('Format ' + formatNames[i] + ' Smoke Test', simpleTestSuite(formatIndex, formatNames[i]));
+
+  // skip running tests on Domino 1 Ad, Domino 3 Ads and Wrapper
+  if ((i === 3) || (i === 5) || (i === 13)) { i++; }
+}
